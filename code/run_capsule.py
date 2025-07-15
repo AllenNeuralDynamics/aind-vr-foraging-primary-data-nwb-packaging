d35589e8068898f8e05b453200999f1760e32d99
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2025-07-15 00:43:50 +0000

    add pulling version from tasklogic file. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/774. fix linting

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -4,13 +4,14 @@ from pathlib import Path
 
 import contraqctor.contract as data_contract
 import pynwb
-import utils
 from aind_behavior_vr_foraging.data_contract import dataset
 from dateutil import parser
 from hdmf_zarr import NWBZarrIO
-from ndx_events import EventsTable, MeaningsTable, NdxEventsNWBFile
+from ndx_events import NdxEventsNWBFile
 from pydantic import Field
 from pydantic_settings import BaseSettings
+
+import utils
 
 logger = logging.getLogger(__name__)
 
@@ -50,17 +51,36 @@ if __name__ == "__main__":
     if not session_json_path:
         raise FileNotFoundError("Primary data asset has no session json file")
     if not data_description_json_path:
-        raise FileNotFoundError("Primary data asset has no data description json")
+        raise FileNotFoundError(
+            "Primary data asset has no data description json"
+        )
 
     with open(session_json_path[0], "r") as f:
         session_json = json.load(f)
     with open(data_description_json_path[0], "r") as f:
         data_description_json = json.load(f)
     logger.info(
-        f"Found primary data {data_description_json["name"]}. Starting acquisition nwb packaging now"
+        f"Found primary data {data_description_json['name']}. \
+        Starting acquisition nwb packaging now"
     )
 
-    vr_foraging_dataset = dataset(primary_data_path[0])
+    # pull version from here - file always assumed to exist at that path
+    task_input_logic_path = (
+        primary_data_path[0] / "behavior" / "Logs" / "tasklogic_input.json"
+    )
+    if not task_input_logic_path.exists():
+        raise FileNotFoundError(
+            f"No task logic input file found at path {task_input_logic_path}"
+        )
+
+    with open(task_input_logic_path, "r") as f:
+        task_input_logic = json.load(f)
+
+    contract_version = task_input_logic["version"]
+    logger.info(f"Using data contract version {contract_version}")
+    vr_foraging_dataset = dataset(
+        primary_data_path[0], version=contract_version
+    )
     exec = vr_foraging_dataset.load_all()  # load tree structure
     streams = tuple(vr_foraging_dataset.iter_all())
     event_data = []  # for adding to events table
@@ -73,16 +93,14 @@ if __name__ == "__main__":
         identifier=data_description_json["subject_id"],
     )
     for stream in streams:
-        if (
-            stream.is_collection
-        ):  # only process leaf nodes which is what will be ultimately packaged into nwb
+        if stream.is_collection:  # only process leaf nodes into nwb
             continue
 
         name = stream.resolved_name.replace("::", ".")
-        name = name[name.index(".") + 1 :]
-        if isinstance(stream.parent, data_contract.harp.HarpDevice) or isinstance(
-            stream, data_contract.csv.Csv
-        ):
+        name = name[name.index(".") + 1:]
+        if isinstance(
+            stream.parent, data_contract.harp.HarpDevice
+        ) or isinstance(stream, data_contract.csv.Csv):
             try:
                 dynamic_table = pynwb.core.DynamicTable.from_dataframe(
                     name=name,
@@ -91,7 +109,9 @@ if __name__ == "__main__":
                 )
                 nwb_file.add_acquisition(dynamic_table)
             except (ValueError, FileNotFoundError) as e:
-                logger.error(f"Failed to load {stream.name} with exception {e}")
+                logger.error(
+                    f"Failed to load {stream.name} with exception {e}"
+                )
         elif isinstance(stream, data_contract.json.SoftwareEvents):
             try:
                 data = utils.clean_dataframe_for_nwb(stream.data.reset_index())
@@ -102,7 +122,8 @@ if __name__ == "__main__":
                 nwb_file.add_acquisition(dynamic_table)
             except (ValueError, FileNotFoundError) as e:
                 logger.error(
-                    f"Failed to get {stream.name} from {stream.parent} with error {e}"
+                    f"Failed to get {stream.name} \
+                    from {stream.parent} with error {e}"
                 )
         elif isinstance(stream, data_contract.json.PydanticModel):
             data = utils.clean_dictionary_for_nwb(stream.data.model_dump())
@@ -113,34 +134,14 @@ if __name__ == "__main__":
                     description=json.dumps(data),
                 )
             )
-    """
-    meanings_table = MeaningsTable(
-        name="event_descriptions",
-        description="Describes meaning of event and the corresponding data",
-    )
-    events_table = EventsTable(
-        name="events",
-        description="Events logged by acquisition workflow",
-        meanings_tables=[meanings_table],
-    )
-    events_table.add_column(
-        name="event_name", description="event logged by acquisition workflow"
-    )
-    events_table.add_column(
-        name="event_data", description="event data from the acquisition workflow"
-    )
 
-    for table in event_data:
-        utils.add_event(events_table, meanings_table, table)
-
-    nwb_file.add_events_table(events_table)
-    """
     nwb_result_path = (
         settings.output_directory / f"{data_description_json['name']}_nwb"
     )
     logger.info(
-        f"Succesfully finished nwb acquisition packaging with timeseries. Writing to disk now at path {nwb_result_path} as zarr"
+        "Succesfully finished nwb acquisition packaging with timeseries."
     )
+    logger.info(f"Writing to disk now at path {nwb_result_path} as zarr")
     with NWBZarrIO(
         (nwb_result_path).as_posix(),
         "w",
--- code/utils.py
+++ code/utils.py
@@ -2,131 +2,17 @@ import json
 import logging
 from datetime import datetime
 from enum import Enum
-from pathlib import Path
 from typing import Any, Dict, List, Union
 
 import numpy as np
 import pandas as pd
-import pynwb
-from contraqctor.contract import Dataset, DataStream
-from ndx_events import EventsTable, MeaningsTable
 
 logger = logging.getLogger(__name__)
-
-
-def add_event(
-    events_table: EventsTable,
-    meanings_table: MeaningsTable,
-    event_data: pynwb.core.DynamicTable,
-) -> None:
-    """
-    Adds event data to the EventsTable and MeaningsTable based on the provided DataFrame.
-
-    Parameters
-    ----------
-    events_table : EventsTable
-        A table where event data is stored. This table will be updated with new event information.
-
-    meanings_table : MeaningsTable
-        A table containing the meanings of events. This table will be updated based on the data provided.
-
-    event_data : pynwb.core.DynamicTable
-        A table containing new event data. Each row corresponds to a new event entry and contains
-        information that needs to be inserted into both the events_table and meanings_table.
-
-    Returns
-    -------
-    None
-    """
-    data = event_data[:]
-    for index, row in data.iterrows():
-        events_table.add_row(
-            timestamp=row["timestamp"], event_name=row["name"], event_data=row["data"]
-        )
-        meanings_table.add_row(
-            value=row["data"],
-            meaning=f"{row['name']} - {event_data.description}",
-        )
-
-
-def get_top_level_stream(
-    streams: Dataset, key_to_match: str = "Behavior"
-) -> DataStream:
-    """
-    Identifies and returns the top-level data stream from a tuple of streams.
-
-    This function assumes there is one such stream that serves as the entry point or parent in the stream hierarchy.
-
-    Parameters
-    ----------
-    streams : tuple of DataStream
-        A tuple containing multiple DataStream objects from which the top-level stream
-        will be identified.
-
-    key_to_match: str
-        The name to match the top-level stream that is desired
-
-    Returns
-    -------
-    DataStream
-        The top-level stream among the input streams.
-    """
-    top_level_stream = None
-    for stream in streams:
-        if (
-            stream.parent.parent is None and stream.name == key_to_match
-        ):  # found the desired top level stream
-            top_level_stream = stream
-            break
-
-    return top_level_stream
-
-
-def get_stream_name(
-    stream: DataStream, top_level_stream: DataStream
-) -> Union[str, None]:
-    """
-    Generates a name for a given data stream relative to a top-level stream. Mainly for naming in the nwb tables.
-
-    This function determines a name that uniquely identifies the `stream` within the context
-    of the `top_level_stream`.
-
-    Parameters
-    ----------
-    stream : DataStream
-        The target data stream for which to generate a name.
-
-    top_level_stream : DataStream
-        The root or top-level data stream that provides context for the name generation.
-
-    Returns
-    -------
-    str
-        The name of the stream with respect to the top-level stream passed in
-    """
-    name = None
-
-    while stream.name != top_level_stream.name:
-        if name is None:
-            name = stream.name
-        else:
-            name = f"{stream.name}.{name}"
-
-        stream = stream.parent
-        if stream is None:
-            return None
-
-    name = f"{top_level_stream.name}.{name}"
-    return name
 
 
 def clean_dataframe_for_nwb(data: pd.DataFrame) -> pd.DataFrame:
     """
     Clean a pandas DataFrame to ensure compatibility with NWB format.
-
-    This function performs:
-    - Converting unsupported data types (e.g., object or mixed types) to strings or appropriate formats.
-    - Replacing NoneType with NaNs
 
     Parameters
     ----------
@@ -155,12 +41,14 @@ def datetime_to_iso_in_dict(
     data: Union[Dict[str, Any], List[Any], datetime, Any],
 ) -> Union[Dict[str, Any], List[Any], str, Any]:
     """
-    Recursively convert all `datetime` objects in a nested dictionary or list to strings.
+    Recursively convert all `datetime` objects in a
+    nested dictionary or list to strings.
 
     Parameters
     ----------
     data : dict or list or datetime or any
-        Input data structure which may contain nested dictionaries, lists, and datetime objects.
+        Input data structure which may contain
+        nested dictionaries, lists, and datetime objects.
 
     Returns
     -------
