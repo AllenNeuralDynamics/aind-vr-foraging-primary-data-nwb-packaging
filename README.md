a31f8f3a6c073f625f8578b4b77332960820688b
arjunsridhar12345 <109561860+arjunsridhar12345@users.noreply.github.com>
2025-08-12 13:57:03 -0700

    Data contract nwb packaging (#1)
    * Added environment.json
    
    * initial commit, linting
    
    * fix writing data types from software events to nwb
    
    * Initial readme description
    
    * update data contract, fix event description, linting. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/699
    
    * update description of meanings table
    
    * remove hardcoded check. remove extra variable for stream name. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/699
    
    * fix filename for torque stream
    
    * update readme for getting json data
    
    * update acquisition fields in readme
    
    * fix typo in readme
    
    * minor update to readme to make more general
    
    * add try except for getting stream
    
    * remove unused classes for attempting hierarchy. simplify functions and main run
    
    * fix json reading
    
    * simplify logic, remove unused functions
    
    * add capsule link to readme
    
    * start addressing PR feedback. update data contract dependency. https://github.com/AllenNeuralDynamics/aind-vr-foraging-primary-data-nwb-packaging/pull/1
    
    * update readme with specific keys that have json data
    
    * remove hardcoded paths and use pydantic base settings. 
    https://github.com/AllenNeuralDynamics/aind-vr-foraging-primary-data-nwb-packaging/pull/1
    
    * add link to data contract and minor restructure
    
    * use generic mount name
    
    * use camel case for settings class name
    
    * use uv to bootstrap dependencies
    
    * simplify output name of raw nwb. remove adding events to raw nwb for now
    
    * add pulling version from tasklogic file. https://github.com/AllenNeuralDynamics/aind-physio-arch/issues/774. fix linting
    
    * revert run and docker file
    
    * put back uv dependency
    
    * put back uv in run.sh
    
    * refactor after PR feedback.
    
    * add subject to nwb
    
    * docs: update readme

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# aind-capsule-template
+# VR Foraging Primary Data NWB Packaging
 
-Basic structure of a capsule to be customized as needed.
+The capsule can be found [here](https://codeocean.allenneuraldynamics.org/capsule/3265591/tree). 
+
+This capsule packages VR Foraging Primary Data into NWB following the file standards listed [here](https://github.com/AllenNeuralDynamics/aind-file-standards/blob/main/file_formats/nwb.md). 
+
+The capsule uses a data contract with relevants paths to raw data to be packaged. An example can be found at the [following](https://github.com/AllenNeuralDynamics/Aind.Behavior.VrForaging/blob/main/src/aind_behavior_vr_foraging/data_contract/v0_5_0.py). 
+
+The output of this packaging is an nwb file with data in the `acquisition` module. Most, if not all of the data in the nwb are stored as `DynamicTables`, a table representation of the various streams from the input data contract. To read the nwb and access the data, the following code snippet can be run - dependencies are `hdmf-zarr`, and `pynwb`.
+
+```
+from hdmf_zarr import NWBZarrIO
+import json
+
+# REPLACE WITH PATH TO NWB
+with NWBZarrIO('path/to/nwb', 'r') as io:
+  nwb = io.read()
+
+keys = list(nwb.acquisition.keys())
+# access one of streams from acquisition
+data = nwb.acquisition[keys[0]]
+print(data)
+data_df = data[:] # gives a dataframe representation of table
+
+# For fetching json files from the data contract, for now, they are stored in a dynamic table but in the description field. The way to recover it from the nwb is as follows:
+json_dict = json.loads(nwb.acquisition[key].description)
+```
+
+The acqusition module in the nwb is structured as follows:
+## 📁 NWBFile: Acqusition Module
+
+This module contains tables from the primary data. Each entry is a `DynamicTable` that can be accessed with the code snippet above. The tables with json information in the description are `Behavior.InputSchemas.Rig`, `Behavior.InputSchemas.Session`, and `Behavior.InputSchemas.TaskLogic`.
+
+### 📑 DynamicTables
+
+- `Behavior.HarpBehavior.AnalogData`
+- `Behavior.HarpBehavior.AssemblyVersion`
+- `Behavior.HarpBehavior.Camera0Frame`
+- `Behavior.HarpBehavior.Camera0Frequency`
+- `Behavior.HarpBehavior.Camera1Frame`
+- `Behavior.HarpBehavior.Camera1Frequency`
+- `Behavior.HarpBehavior.ClockConfiguration`
+- `Behavior.HarpBehavior.CoreVersionHigh`
+- `...`
--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,158 @@
-""" top level run script """
+import json
+import logging
+from pathlib import Path
 
-def run():
-    """ basic run function """
-    pass
+import contraqctor.contract as data_contract
+import pynwb
+from aind_behavior_vr_foraging.data_contract import dataset
+from aind_nwb_utils.utils import get_subject_nwb_object
+from dateutil import parser
+from hdmf_zarr import NWBZarrIO
+from ndx_events import NdxEventsNWBFile
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
-if __name__ == "__main__": run()
+import utils
+
+logger = logging.getLogger(__name__)
+
+
+class VRForagingSettings(BaseSettings, cli_parse_args=True):
+    """
+    Settings for VR Foraging Primary Data NWB Packaging
+    """
+
+    input_directory: Path = Field(
+        default=Path("/data/"), description="Directory where data is"
+    )
+    output_directory: Path = Field(
+        default=Path("/results/"), description="Output directory"
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    settings = VRForagingSettings()
+    primary_data_path = tuple(settings.input_directory.glob("*"))
+    if not primary_data_path:
+        raise FileNotFoundError("No primary data asset attached")
+
+    if len(primary_data_path) > 1:
+        raise ValueError(
+            "Multiple primary data assets attached. Only single asset needed"
+        )
+
+    session_json_path = tuple(settings.input_directory.glob("*/session.json"))
+    data_description_json_path = tuple(
+        settings.input_directory.glob("*/data_description.json")
+    )
+    subject_json_path = tuple(settings.input_directory.glob("*/subject.json"))
+    if not session_json_path:
+        raise FileNotFoundError("Primary data asset has no session json file")
+    if not data_description_json_path:
+        raise FileNotFoundError(
+            "Primary data asset has no data description json"
+        )
+
+    if not subject_json_path:
+        raise FileNotFoundError("Primary data asset has no subject json")
+
+    with open(session_json_path[0], "r") as f:
+        session_json = json.load(f)
+    with open(data_description_json_path[0], "r") as f:
+        data_description_json = json.load(f)
+    with open(subject_json_path[0], "r") as f:
+        subject_json = json.load(f)
+    logger.info(
+        f"Found primary data {data_description_json['name']}. \
+        Starting acquisition nwb packaging now"
+    )
+
+    # pull version from here - file always assumed to exist at that path
+    task_input_logic_path = (
+        primary_data_path[0] / "behavior" / "Logs" / "tasklogic_input.json"
+    )
+    if not task_input_logic_path.exists():
+        raise FileNotFoundError(
+            f"No task logic input file found at path {task_input_logic_path}"
+        )
+
+    with open(task_input_logic_path, "r") as f:
+        task_input_logic = json.load(f)
+
+    contract_version = task_input_logic["version"]
+    logger.info(f"Using data contract version {contract_version}")
+    vr_foraging_dataset = dataset(
+        primary_data_path[0], version=contract_version
+    )
+    exec = vr_foraging_dataset["Behavior"].load_all()  # load tree structure
+    streams = tuple(vr_foraging_dataset.iter_all())
+    event_data = []  # for adding to events table
+
+    # using this ndx object for events table
+    nwb_file = NdxEventsNWBFile(
+        session_id=data_description_json["name"],
+        session_description=f"Version {contract_version}",
+        session_start_time=parser.parse(session_json["session_start_time"]),
+        identifier=data_description_json["subject_id"],
+        subject=get_subject_nwb_object(data_description_json, subject_json),
+    )
+    for stream in streams:
+        if stream.is_collection:  # only process leaf nodes into nwb
+            continue
+
+        name = stream.resolved_name.replace("::", ".")
+        name = name[name.index(".") + 1:]
+        if isinstance(stream, data_contract.harp.HarpRegister) or isinstance(
+            stream, data_contract.csv.Csv
+        ):
+            try:
+                dynamic_table = pynwb.core.DynamicTable.from_dataframe(
+                    name=name,
+                    table_description=stream.description,
+                    df=stream.data.reset_index(),
+                )
+                nwb_file.add_acquisition(dynamic_table)
+            except (ValueError, FileNotFoundError) as e:
+                logger.error(
+                    f"Failed to load {stream.name} with exception {e}"
+                )
+        elif isinstance(stream, data_contract.json.SoftwareEvents):
+            try:
+                data = utils.clean_dataframe_for_nwb(stream.data.reset_index())
+                dynamic_table = pynwb.core.DynamicTable.from_dataframe(
+                    name=name, table_description=stream.description, df=data
+                )
+                event_data.append(dynamic_table)
+                nwb_file.add_acquisition(dynamic_table)
+            except (ValueError, FileNotFoundError) as e:
+                logger.error(
+                    f"Failed to get {stream.name} \
+                    from {stream.parent} with error {e}"
+                )
+        elif isinstance(stream, data_contract.json.PydanticModel):
+            data = utils.clean_dictionary_for_nwb(stream.data.model_dump())
+
+            nwb_file.add_acquisition(
+                pynwb.core.DynamicTable(
+                    name=name,
+                    description=json.dumps(data),
+                )
+            )
+
+    nwb_result_path = (
+        settings.output_directory / f"{data_description_json['name']}_nwb"
+    )
+    logger.info(
+        "Succesfully finished nwb acquisition packaging with timeseries."
+    )
+    logger.info(f"Writing to disk now at path {nwb_result_path} as zarr")
+    with NWBZarrIO(
+        (nwb_result_path).as_posix(),
+        "w",
+    ) as io:
+        io.write(nwb_file)
+    logger.info(f"NWB zarr successfully written to path {nwb_result_path}")
