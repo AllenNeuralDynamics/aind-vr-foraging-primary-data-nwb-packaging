30b41ccb61b65739a73f9f99eedc19e03b26af5f
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2026-03-03 23:20:26 +0000

    feat: restore entire processing

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -9,7 +9,7 @@ from aind_behavior_vr_foraging.data_contract import dataset
 from aind_data_schema.components.identifiers import Code
 from aind_data_schema.core.processing import DataProcess, ProcessStage
 from aind_data_schema_models.process_names import ProcessName
-from aind_nwb_utils.utils import create_base_nwb_file, get_subject_nwb_object
+from aind_nwb_utils.utils import create_base_nwb_file
 from dateutil import parser
 from hdmf_zarr import NWBZarrIO
 from ndx_events import NdxEventsNWBFile
@@ -56,27 +56,27 @@ if __name__ == "__main__":
             "Multiple primary data assets attached. Only single asset needed"
         )
 
-    # acquisition_json_path = tuple(settings.input_directory.glob("*/acquisition.json"))
+    acquisition_json_path = tuple(settings.input_directory.glob("*/acquisition.json"))
     data_description_json_path = tuple(
         settings.input_directory.glob("*/data_description.json")
     )
-    # subject_json_path = tuple(settings.input_directory.glob("*/subject.json"))
-    # if not acquisition_json_path:
-    #     raise FileNotFoundError("Primary data asset has no acquisition json file")
+    subject_json_path = tuple(settings.input_directory.glob("*/subject.json"))
+    if not acquisition_json_path:
+        raise FileNotFoundError("Primary data asset has no acquisition json file")
     if not data_description_json_path:
         raise FileNotFoundError(
             "Primary data asset has no data description json"
         )
 
-    # if not subject_json_path:
-    #     raise FileNotFoundError("Primary data asset has no subject json")
+    if not subject_json_path:
+        raise FileNotFoundError("Primary data asset has no subject json")
 
-    # with open(acquisition_json_path[0], "r") as f:
-    #     acquisition_json = json.load(f)
+    with open(acquisition_json_path[0], "r") as f:
+        acquisition_json = json.load(f)
     with open(data_description_json_path[0], "r") as f:
         data_description_json = json.load(f)
-    # with open(subject_json_path[0], "r") as f:
-    #     subject_json = json.load(f)
+    with open(subject_json_path[0], "r") as f:
+        subject_json = json.load(f)
     logger.info(
         f"Found primary data {data_description_json['name']}. \
         Starting acquisition nwb packaging now"
@@ -100,69 +100,61 @@ if __name__ == "__main__":
     vr_foraging_dataset = dataset(
         primary_data_path[0], version=contract_version
     )
-    #exec = vr_foraging_dataset["Behavior"].load_all()  # load tree structure
-    #streams = tuple(vr_foraging_dataset.iter_all())
+    exec = vr_foraging_dataset["Behavior"].load_all()  # load tree structure
+    streams = tuple(vr_foraging_dataset.iter_all())
  
     processor = DatasetProcessor(vr_foraging_dataset, primary_data_path[0], raise_on_error=False)
     processed_sites = processor.process()
 
-    # # using this ndx object for events table
-    # nwb_file = NdxEventsNWBFile(
-    #     session_id=data_description_json["name"],
-    #     session_description=f"Version {contract_version}",
-    #     session_start_time=parser.parse(acquisition_json["acquisition_start_time"]),
-    #     identifier=data_description_json["subject_id"],
-    #     subject=get_subject_nwb_object(data_description_json, subject_json),
-    # )
     nwb_file = create_base_nwb_file(primary_data_path[0])
-    # for stream in streams:
-    #     if stream.is_collection:  # only process leaf nodes into nwb
-    #         continue
+    for stream in streams:
+        if stream.is_collection:  # only process leaf nodes into nwb
+            continue
 
-    #     name = stream.resolved_name.replace("::", ".")
-    #     name = name[name.index(".") + 1:]
-    #     if isinstance(stream, data_contract.harp.HarpRegister) or isinstance(
-    #         stream, data_contract.csv.Csv
-    #     ):
-    #         try:
-    #             dynamic_table = pynwb.core.DynamicTable.from_dataframe(
-    #                 name=name,
-    #                 table_description=stream.description,
-    #                 df=stream.data.reset_index(),
-    #             )
-    #             nwb_file.add_acquisition(dynamic_table)
-    #         except (ValueError, FileNotFoundError) as e:
-    #             logger.error(
-    #                 f"Failed to load {stream.name} with exception {e}"
-    #             )
-    #     elif isinstance(stream, data_contract.json.SoftwareEvents):
-    #         try:
-    #             data = utils.clean_dataframe_for_nwb(stream.data.reset_index())
-    #             dynamic_table = pynwb.core.DynamicTable.from_dataframe(
-    #                 name=name, table_description=stream.description, df=data
-    #             )
-    #             event_data.append(dynamic_table)
-    #             nwb_file.add_acquisition(dynamic_table)
-    #         except (ValueError, FileNotFoundError) as e:
-    #             logger.error(
-    #                 f"Failed to get {stream.name} \
-    #                 from {stream.parent} with error {e}"
-    #             )
-    #     elif isinstance(stream, data_contract.json.PydanticModel):
-    #         try:
-    #             data = utils.clean_dictionary_for_nwb(stream.data.model_dump())
+        name = stream.resolved_name.replace("::", ".")
+        name = name[name.index(".") + 1:]
+        if isinstance(stream, data_contract.harp.HarpRegister) or isinstance(
+            stream, data_contract.csv.Csv
+        ):
+            try:
+                dynamic_table = pynwb.core.DynamicTable.from_dataframe(
+                    name=name,
+                    table_description=stream.description,
+                    df=stream.data.reset_index(),
+                )
+                nwb_file.add_acquisition(dynamic_table)
+            except (ValueError, FileNotFoundError) as e:
+                logger.error(
+                    f"Failed to load {stream.name} with exception {e}"
+                )
+        elif isinstance(stream, data_contract.json.SoftwareEvents):
+            try:
+                data = utils.clean_dataframe_for_nwb(stream.data.reset_index())
+                dynamic_table = pynwb.core.DynamicTable.from_dataframe(
+                    name=name, table_description=stream.description, df=data
+                )
+                event_data.append(dynamic_table)
+                nwb_file.add_acquisition(dynamic_table)
+            except (ValueError, FileNotFoundError) as e:
+                logger.error(
+                    f"Failed to get {stream.name} \
+                    from {stream.parent} with error {e}"
+                )
+        elif isinstance(stream, data_contract.json.PydanticModel):
+            try:
+                data = utils.clean_dictionary_for_nwb(stream.data.model_dump())
 
-    #             nwb_file.add_acquisition(
-    #                 pynwb.core.DynamicTable(
-    #                     name=name,
-    #                     description=json.dumps(data),
-    #                 )
-    #             )
-    #         except (ValidationError) as e:
-    #             logger.error(
-    #                 f"Failed to get {stream.name} \
-    #                 from {stream.parent} with error {e}"
-    #             )
+                nwb_file.add_acquisition(
+                    pynwb.core.DynamicTable(
+                        name=name,
+                        description=json.dumps(data),
+                    )
+                )
+            except (ValidationError) as e:
+                logger.error(
+                    f"Failed to get {stream.name} \
+                    from {stream.parent} with error {e}"
+                )
 
     for field_name, field in Site.model_fields.items():
         if field_name in ["start_time", "stop_time"]:
