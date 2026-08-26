589b49c9b2ec6a42cf1a29b950a0aea23270b478
arjunsridhar12345 <109561860+arjunsridhar12345@users.noreply.github.com>
2026-08-26 14:26:47 -0700

    refactor to align with upstream package (#24)
    * refactor: move provenance to output parameters and move session parquets to subfolder
    
    * delete sandbox notebook
    
    * feat: add process name
    
    * build: update packaging library version
    
    * refactor: simplify to use entrypoint function
    
    * explicitly set write_parquet

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -4,9 +4,9 @@ import os
 from datetime import UTC, datetime
 from pathlib import Path
 
-from aind_behavior_vr_foraging_packaging.nwb_file import NwbSession
-from aind_behavior_vr_foraging_packaging.pipeline import create_processors
+from aind_behavior_vr_foraging.data_contract import dataset as load_dataset
 from aind_behavior_vr_foraging_packaging.pipeline import process_session
+from aind_behavior_vr_foraging_packaging._provenance import PackagingProvenance
 from aind_data_schema.components.identifiers import Code
 from aind_data_schema.core.processing import DataProcess, ProcessStage
 from aind_data_schema_models.process_names import ProcessName
@@ -63,47 +63,34 @@ def run() -> None:
     logging.info("Begin processing...", extra={"event_type": "stage_start"})
     logging.info(
         f"Found primary data {data_description_json['name']}. \
-        Starting acquisition nwb packaging now"
+        Starting packaging now"
     )
 
-    nwb_session = NwbSession(primary_data_path[0])
-    processors = create_processors(nwb_session.dataset)
-    nwb_session.run(*processors)
-
-    logging.info(
-        "Successfully finished nwb packaging."
+    dataset = load_dataset(primary_data_path[0])
+    process_session(
+        dataset=dataset, 
+        output_dir=settings.output_directory, 
+        write_parquet=True,
+        write_nwb=True
     )
-
-    logging.info(
-        "Generating parquet results"
-    )
-    process_session(nwb_session.dataset, output_dir=settings.output_directory, processors=processors)
-    logging.info(
-        f"Successfully wrote parquet files to {settings.output_directory}"
-    )
-
-    nwb_result_path = settings.output_directory / "behavior.nwb.zarr"
-    logging.info(f"Writing nwb to disk now at path {nwb_result_path} as zarr")
-    nwb_session.write_nwb_zarr(nwb_result_path)
 
     end_process_time = datetime.now(tz=UTC)
-    provenance = nwb_session.provenance
+    provenance = PackagingProvenance.build(dataset)
     data_process = DataProcess(
         start_date_time=start_process_time,
         end_date_time=end_process_time,
+        name="primary-nwb-packaging-vr-foraging",
         stage=ProcessStage.PROCESSING,
         process_type=ProcessName.PIPELINE,
         experimenters=["Bruno Cruz", "Tiffany Ona", "Arjun Sridhar"],
         code=Code(
             url=_PACKAGING_GITHUB_URL,
-            version=provenance["packaging_version"]
+            version=provenance.packaging_version
         ),
-        output_parameters={},
+        output_parameters=provenance.model_dump(),
         pipeline_name=_PIPELINE_NAME,
-        notes=json.dumps(
-            nwb_session.nwb_file.was_generated_by
-        )
     )
+    logging.info(f"Succesfully wrote nwb and parquet files to {settings.output_directory}")
     with open(settings.output_directory / "data_process.json", "w") as f:
         f.write(data_process.model_dump_json(indent=4))
     logging.info("Pipeline stage completed", extra={"event_type": "stage_complete"})
