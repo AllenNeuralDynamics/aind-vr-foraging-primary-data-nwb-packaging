d162f9d0f568bb6d0d74cbc36686f019b46c038a
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2026-09-22 15:33:32 +0000

    prepare for reprocessing

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -20,7 +20,7 @@ from pydantic_settings import BaseSettings
 
 _PACKAGING_GITHUB_URL="https://github.com/AllenNeuralDynamics/Aind.Behavior.VrForaging.Packaging.git"
 _PROCESS_NAME = "primary-nwb-packaging-vr-foraging"
-_FROZEN_METADATA_ASSET = "vr_paper_raw_metadata_09-02-2026"
+_FROZEN_METADATA_ASSET = "vr_sessions_raw_metadata_snapshot_09-22-2026"
 _EXPERIMENTERS = ["Bruno Cruz", "Tiffany Ona", "Arjun Sridhar"]
 _FILES_TO_COPY = [
     "acquisition",
